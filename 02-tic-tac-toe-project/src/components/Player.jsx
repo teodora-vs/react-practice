45e0aba5f97aa812b9fcf1a3a69c2ccff239277c
teodora-vs <teodorav1415@students.softuni.bg>
2025-11-01 22:21:50 +0200

    editable player names achieved through useState function

--- 02-tic-tac-toe-project/src/components/Player.jsx
+++ 02-tic-tac-toe-project/src/components/Player.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        setIsEditing(true);
+    function handleEditClick() {  
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) { 
+        setPlayerName(event.target.value);
+    }
 
-    if(isEditing) {
-        playerName = <input type="text" required />;
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange}/>
+        );
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
